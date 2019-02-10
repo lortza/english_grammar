a2689d8884db7f554c9b7f4f60a6d65fb03b7089
Anne Richardson <richardson.ae@gmail.com>
2019-02-10 16:38:50 -0600

    WIP verb construction

--- verb.rb
+++ verb.rb
@@ -1,11 +1,42 @@
 require './grammar'
 
 class Verb < Grammar
-  attr_reader :name, :tense, :person
+  attr_reader :infinitive,
+              :tense,
+              :person,
+              :first_person_singular,
+              :first_person_plural,
+              :second_person_singular,
+              :second_person_plural,
+              :third_person_singular,
+              :third_person_plural
 
-  def initialize(name:, tense:, person:)
-    @name = name
+  def initialize(infinitive:,
+                 tense:,
+                 first_person_singular:,
+                 first_person_plural:,
+                 second_person_singular:,
+                 second_person_plural:,
+                 third_person_singular:,
+                 third_person_plural:)
+    @infinitive = infinitive
     @tense = tense
-    @person = person
+    @first_person_singular = first_person_singular
+    @first_person_plural = first_person_plural
+    @second_person_singular = second_person_singular
+    @second_person_plural = second_person_plural
+    @third_person_singular = third_person_singular
+    @third_person_plural = third_person_plural
   end
 end
+
+p Verb.new(
+  infinitive: 'to imagine',
+  tense: 'present',
+  first_person_singular: 'imagine',
+  first_person_plural: 'imagine',
+  second_person_singular: 'imagines',
+  second_person_plural: 'imagine',
+  third_person_singular: 'imagine',
+  third_person_plural: 'imagine'
+)
