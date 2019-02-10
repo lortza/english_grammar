d67fbda97040622ec384951703e6de9b31ecb6d0
Anne Richardson <richardson.ae@gmail.com>
2019-02-10 12:31:45 -0600

    WIP needs third person

---
+++ possessive_pronoun.rb
@@ -0,0 +1,63 @@
+require './pronoun'
+
+# Examples: mine, ours, yours, his, hers, its, theirs
+# Context: That cat is mine.
+
+class PossessivePronoun < Pronoun
+  attr_reader :name, :plurality, :gender, :person
+
+  def initialize(name:, plurality:, gender: 'neutral', person:)
+    super(name:name, plurality: plurality, gender: gender)
+    @person = person
+  end
+
+  class << self
+    def all
+      build_all
+    end
+
+
+    private
+    def build_all
+      all = []
+
+      # First-person
+      all << self.new(
+        name: 'mine',
+        person: 'second',
+        plurality: 'singular',
+        gender: 'neutral'
+      )
+
+      all << self.new(
+        name: 'ours',
+        person: 'first',
+        plurality: 'plural',
+        gender: 'neutral'
+      )
+
+      # Second-person
+      all << self.new(
+        name: 'yours',
+        person: 'second',
+        plurality: 'singular',
+        gender: 'neutral'
+      )
+
+      all << self.new(
+        name: 'yours',
+        person: 'second',
+        plurality: 'plural',
+        gender: 'neutral'
+      )
+
+      # Third-person
+
+
+
+    end
+  end
+end
+
+
+p PossessivePronoun.all
